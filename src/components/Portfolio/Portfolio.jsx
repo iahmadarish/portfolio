d2037c47a44b3084b670017bf281c19c58758b19
Md ishaq ahmad <143544402+iahmadarish@users.noreply.github.com>
2024-03-14 06:08:58 +0600

    Add files via upload

---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,43 @@
+import React, { useContext } from "react";
+import "./Portfolio.css";
+import { Swiper, SwiperSlide } from "swiper/react"
+import "swiper/css";
+import travel from "../../img/bangladesh.png";
+import Ecommerce from "../../img/cara.png";
+import HOC from "../../img//h2o.png";
+import MusicApp from "../../img/pertroil.png";
+import { themeContext } from "../../Context";
+const Portfolio = () => {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+  return (
+    <div className="portfolio" id="portfolio">
+      {/* heading */}
+      <span style={{color: darkMode?'white': ''}}>Recent Projects</span>
+      <span className="">React and Next js</span>
+
+      {/* slider */}
+      <Swiper
+        spaceBetween={30}
+        slidesPerView={3}
+        grabCursor={true}
+        className="portfolio-slider"
+      >
+        <SwiperSlide>
+          <img src={travel} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Ecommerce} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={MusicApp} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={HOC} alt="" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default Portfolio;
